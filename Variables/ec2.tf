e94232c97c2f6bcd7ffea7a23c2baa436316a0fd
jpdaws-cloud-github <jpreddymca@gmail.com>
2024-09-17 18:55:25 +0530

    ansible-main

---
+++ Variables/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id]
+
+  tags = {
+    Name = var.tags
+  }
+}
