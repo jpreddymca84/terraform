20d36f39d17da143294cdc318d718f43cc24b5e3
jpdaws-cloud-github <jpreddymca@gmail.com>
2024-09-19 19:04:59 +0530

    ansible-main

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "example" {
+data "aws_ami" "centos8" {
   owners           = ["973714476881"]
   most_recent      = true
   
@@ -16,4 +16,28 @@ data "aws_ami" "example" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+}
+
+data "aws_ami" "aws-linux-2" {
+  owners           = ["amazon"]
+  most_recent      = true
+  
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
 }
